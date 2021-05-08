ba16ba56b0f9bd39e11f55e37c8393a5fa47cadb
LinShengfeng-code <lsf296100178@gmail.com>
2021-05-08 11:01:00 +0800

    Add regular expression in WordAnalyse

--- src/com/lsf/WordAnalysis.java
+++ src/com/lsf/WordAnalysis.java
@@ -1,11 +1,11 @@
 package com.lsf;
 
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Set;
+import java.util.regex.*;
 
 public class WordAnalysis {
+    public String pattern = "^([$|a-z|A-Z][$|a-z|A-Z|0-9|_]*)||(_[$|a-z|A-Z|0-9|_]*[$|a-z|A-Z|0-9]+)";
     //key_word is used to store the key words.
     private String key_word[] = {"return","void","main","break","continue","include","begin","end","if","else","switch","while"};
     private char special_characters[] = {'\n', '\t', ' ', '\r'};
@@ -18,6 +18,10 @@ public class WordAnalysis {
         }
         return false;
     }
+    private boolean isIdentifier(String word){
+        boolean isMatched = Pattern.matches(pattern,word);
+        return isMatched;
+    }
     private boolean isDigit(char digit){
         if(digit>='0'&&digit<='9')return true;
         return false;
@@ -29,7 +33,7 @@ public class WordAnalysis {
         return false;
     }
     private boolean isLetter(char letter){
-        if((letter>='a'&&letter<='z')||(letter>='A'&&letter<='Z'))return true;
+        if((letter>='a'&&letter<='z')||(letter>='A'&&letter<='Z')||(letter == '$')||(letter == '_'))return true;
         return false;
     }
     void analysis(char[] chars){
@@ -46,10 +50,12 @@ public class WordAnalysis {
                 i--;
                 //if the current_word is a key word
                 if(isKeyWord(current_word)){
-                    System.out.println(current_word+"\t4"+"\tkeyword");
+                    System.out.println(current_word+"\t4"+"\tkeyword 关键字");
                 }
                 //if the current_word is an identifier
-                else System.out.println(current_word+"\t4"+"\tidentifier");
+                else if(isIdentifier(current_word)) System.out.println(current_word+"\t4"+"\tidentifier 标识符");
+                //not a legal identifier
+                else System.out.println(current_word+"\t6\tNo Identifier 非标识符");
             }
             else if(isDigit(current_char)||current_char == '.'){
                 while(isDigit(current_char)||(current_char == '.'&&isDigit(chars[++i]))){
@@ -59,14 +65,14 @@ public class WordAnalysis {
                 }
                 i--;
                 //the current_word is digit
-                System.out.println(current_word+"\t5"+"\tnumber");
+                System.out.println(current_word+"\t5"+"\tnumber 常数");
             }
             else switch(current_char){
                     case '+':
                     case '-':
                     case '*':
                     case '/':
-                        System.out.println(current_char+"\t2"+"\toperator");
+                        System.out.println(current_char+"\t2"+"\toperator 操作符");
                         break;
                     case '(':
                     case ')':
@@ -74,41 +80,41 @@ public class WordAnalysis {
                     case ']':
                     case '{':
                     case '}':
-                        System.out.println(current_char+"\t3"+"\tdelimiter");
+                        System.out.println(current_char+"\t3"+"\tdelimiter 分隔符");
                         break;
                     case '=':{
                         current_char = chars[++i];
-                        if(current_char == '=')System.out.println("==\t2\toperator");
+                        if(current_char == '=')System.out.println("==\t2\toperator 操作符");
                         else{
-                            System.out.println("=\t2\toperator");
+                            System.out.println("=\t2\toperator 操作符");
                             i--;
                         }
                     }break;
                     case ':':{
                         current_char = chars[++i];
-                        if(current_char == '=')System.out.println(":=\t2\toperator");
+                        if(current_char == '=')System.out.println(":=\t2\toperator 操作符");
                         else{
-                            System.out.println(":\t2\toperator");
+                            System.out.println(":\t2\toperator 操作符");
                             i--;
                         }
                     }break;
                     case '>':{
                         current_char = chars[++i];
-                        if(current_char == '=')System.out.println(">=\t2\toperator");
+                        if(current_char == '=')System.out.println(">=\t2\toperator 操作符");
                         else{
-                            System.out.println(">\t2\toperator");
+                            System.out.println(">\t2\toperator 操作符");
                             i--;
                         }
                     }break;
                     case '<':{
                         current_char = chars[++i];
-                        if(current_char == '=')System.out.println("<=\t2\toperator");
+                        if(current_char == '=')System.out.println("<=\t2\toperator 操作符");
                         else{
-                            System.out.println("<\t2\toperator");
+                            System.out.println("<\t2\toperator 操作符");
                             i--;
                         }
                     }break;
-                    default:System.out.println(current_char+"\t6\tNo Identifier");
+                    default:System.out.println(current_char+"\t6\tNo Identifier 非标识符");
             }
         }
     }
